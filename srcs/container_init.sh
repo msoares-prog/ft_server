3cc52ec9ada6010d7b76267604ec234cb7729839
msoares <mahh.soares3@gmail.com>
2020-04-04 17:15:03 -0300

    small fix

--- srcs/container_init.sh
+++ srcs/container_init.sh
@@ -17,9 +17,7 @@ cp localhost.crt /etc/ssl/certs/localhost.crt
 cp localhost.key /etc/ssl/private/localhost.key
 mv ./tmp/localhost.conf /etc/nginx/sites-available/localhost
 ln -s /etc/nginx/site-available/localhost /etc/nginx/sites-enabled/localhost
-echo -e "127.0.0.1      localhost" >> /etc/hosts
 rm -rf /etc/nginx/sites-enabled/default
-chmod 600 /etc/ssl/certs/localhost.crt /etc/ssl/private/localhost.key
 
 # conf MySQL
 echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
@@ -32,7 +30,6 @@ mkdir /var/www/localhost/phpmyadmin
 wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
 tar -xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz --strip-components 1 -C /var/www/localhost/phpmyadmin
 mv ./tmp/phpmyadmin.inc.php /var/www/localhost/phpmyadmin/config.inc.php
-mysql phpmyadmin < ./tmp/phpmyadmin.sql
 
 #wp
 cd /tmp/
@@ -40,8 +37,7 @@ wget -c https://wordpress.org/latest.tar.gz
 tar -xvzf latest.tar.gz
 mv wordpress/ /var/www/localhost
 mv /tmp/wp-config.php /var/www/localhost/wordpress
-mysql wordpress < ./tmp/wordpress.sql
 
 service php7.3-fpm start
 service nginx start
-
+bash
